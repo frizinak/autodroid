4a4d037db0bfa96e597c9f88288786816d4d0cfb
Kobe Lipkens <kobelipkens@gmail.com>
2022-01-26 23:44:30 +0100

    auto: better caching and nesting tests

--- auto/behavior.go
+++ auto/behavior.go
@@ -3,12 +3,46 @@ package auto
 import (
 	"fmt"
 	"image"
-	"strings"
 )
 
 type Result struct {
 	Match bool
 	image.Rectangle
+}
+
+type ID string
+
+type Results map[ID]Result
+
+func (r Results) Set(t TestWithResult, res Result) { r[t.ID()] = res }
+func (r Results) Get(id ID) (Result, bool)         { res, ok := r[id]; return res, ok }
+func (r Results) Match(id ID) bool                 { return r[id].Match }
+
+func (r Results) Must(id ID) Result {
+	res, ok := r[id]
+	if !ok || !res.Match {
+		panic(fmt.Sprintf("results for %s does not match", id))
+	}
+	return res
+}
+
+func NewResults() Results { return make(Results) }
+
+func GetOrTest(t Test, search *ImageSearch, results Results) bool {
+	twr, ok := t.(TestWithResult)
+	if !ok {
+		return t.Test(search, results)
+	}
+
+	id := twr.ID()
+	r, ok := results.Get(id)
+	if !ok && t.Test(search, results) {
+		return true
+	}
+	if ok && r.Match {
+		return true
+	}
+	return false
 }
 
 type Behaviors struct {
@@ -21,15 +55,20 @@ func NewBehaviors(behaviors ...Behavior) *Behaviors {
 }
 
 func (b *Behaviors) Do(search *ImageSearch) error {
-	cache := make(map[string]Result)
+	results := NewResults()
 	state := b.state
-	b.state = &State{}
-	if err := state.DoNext(search, cache); err != nil {
+	var err error
+	b.state, err = state.DoNext(search, results)
+	if err != nil {
 		return err
 	}
 
+	if b.state.Stopped() {
+		return nil
+	}
+
 	for _, bh := range b.list {
-		if err := bh.Do(b.state, search, cache); err != nil {
+		if err := bh.Do(b.state, search, results); err != nil {
 			return err
 		}
 		if b.state.Stopped() {
@@ -41,97 +80,132 @@ func (b *Behaviors) Do(search *ImageSearch) error {
 }
 
 type Test interface {
-	ID() string
-	Test(*ImageSearch) Result
+	Test(*ImageSearch, Results) bool
+}
+
+type TestWithResult interface {
+	ID() ID
+	Test
 }
 
 type SubImgTest struct {
-	Uniq      string
+	Uniq      ID
 	Img       image.Image
 	Tolerance uint8
 	Region    image.Rectangle
 }
 
-func (s SubImgTest) ID() string {
-	return fmt.Sprintf("%s - %d - %s", s.Uniq, s.Tolerance, s.Region.String())
-}
+func (s SubImgTest) ID() ID { return s.Uniq }
 
-func (s SubImgTest) Test(search *ImageSearch) Result {
-	r := Result{}
-	res := search.Search(s.Region, s.Img, s.Tolerance)
-	if len(res) == 0 {
-		return r
+func (s SubImgTest) Test(search *ImageSearch, res Results) bool {
+	sr := search.Search(s.Region, s.Img, s.Tolerance)
+	var r Result
+	if len(sr) != 0 {
+		r.Match = true
+		r.Rectangle = sr[0].Rectangle
 	}
-	r.Match = true
-	r.Rectangle = res[0].Rectangle
-	return r
+	res.Set(s, r)
+	return r.Match
 }
 
-func NewSubImgTest(id string, img image.Image, tolerance uint8, region image.Rectangle) SubImgTest {
+func NewSubImgTest(id ID, img image.Image, tolerance uint8, region image.Rectangle) SubImgTest {
 	return SubImgTest{Uniq: id, Img: img, Tolerance: tolerance, Region: region}
 }
 
-type TestCallback func(*ImageSearch) Result
+type TestCallback func(CallbackTest, *ImageSearch, Results) bool
 
 type CallbackTest struct {
-	Uniq string
+	Uniq ID
 	cb   TestCallback
 }
 
-func (c CallbackTest) ID() string                      { return c.Uniq }
-func (c CallbackTest) Test(search *ImageSearch) Result { return c.cb(search) }
+func (c CallbackTest) ID() ID                                   { return c.Uniq }
+func (c CallbackTest) Test(search *ImageSearch, r Results) bool { return c.cb(c, search, r) }
 
-func NewCallbackTest(id string, cb TestCallback) CallbackTest {
+func NewCallbackTest(id ID, cb TestCallback) CallbackTest {
 	return CallbackTest{Uniq: id, cb: cb}
+}
+
+type SimpleTestCallback func(*ImageSearch) Result
+
+type SimpleCallbackTest struct {
+	Uniq ID
+	cb   SimpleTestCallback
+}
+
+func (c SimpleCallbackTest) ID() ID { return c.Uniq }
+func (c SimpleCallbackTest) Test(search *ImageSearch, r Results) bool {
+	res := c.cb(search)
+	r.Set(c, res)
+	return res.Match
+}
+
+func NewSimpleTest(id ID, cb SimpleTestCallback) SimpleCallbackTest {
+	return SimpleCallbackTest{Uniq: id, cb: cb}
 }
 
 type TestGroup struct {
 	Tests []Test
 }
 
-func (g TestGroup) ID() string {
-	n := make([]string, len(g.Tests))
-	for i, t := range g.Tests {
-		n[i] = t.ID()
-	}
-	return strings.Join(n, "\n")
-}
-
 type OrTest struct {
 	TestGroup
 }
 
-func (or OrTest) Test(search *ImageSearch) Result {
+func (or OrTest) Test(search *ImageSearch, results Results) bool {
 	for _, t := range or.Tests {
-		res := t.Test(search)
-		if res.Match {
-			return res
+		if GetOrTest(t, search, results) {
+			return true
 		}
+
 	}
-	return Result{}
+	return false
 }
 
 func NewOrTest(tests ...Test) OrTest {
 	return OrTest{TestGroup{Tests: tests}}
 }
 
+type NotTest struct {
+	T Test
+}
+
+func (not NotTest) Test(search *ImageSearch, results Results) bool {
+	return !not.T.Test(search, results)
+}
+
+func NewNotTest(test Test) NotTest {
+	return NotTest{T: test}
+}
+
 type AndTest struct {
 	TestGroup
 }
 
-func (and AndTest) Test(search *ImageSearch) Result {
-	var res Result
+func (and AndTest) Test(search *ImageSearch, results Results) bool {
 	for _, t := range and.Tests {
-		res = t.Test(search)
-		if !res.Match {
-			return res
+		if !GetOrTest(t, search, results) {
+			return false
 		}
 	}
-	return res
+	return true
 }
 
 func NewAndTest(tests ...Test) AndTest {
 	return AndTest{TestGroup{Tests: tests}}
+}
+
+type AlwaysTest struct {
+	T Test
+}
+
+func (a AlwaysTest) Test(search *ImageSearch, results Results) bool {
+	GetOrTest(a.T, search, results)
+	return true
+}
+
+func NewAlwaysTest(test Test) AlwaysTest {
+	return AlwaysTest{T: test}
 }
 
 type State struct {
@@ -148,52 +222,41 @@ func (s *State) Next(b Behavior) {
 	s.behaviors = append(s.behaviors, b)
 }
 
-func (s *State) DoNext(search *ImageSearch, cache map[string]Result) error {
+func (s *State) DoNext(search *ImageSearch, results Results) (*State, error) {
 	if len(s.behaviors) == 0 {
-		return nil
+		return &State{}, nil
 	}
 
 	state := &State{}
 	for _, b := range s.behaviors {
-		if err := b.Do(state, search, cache); err != nil {
-			return err
+		if err := b.Do(state, search, results); err != nil {
+			return state, err
 		}
 		if state.Stopped() {
 			break
 		}
 	}
 
-	return state.DoNext(search, cache)
+	return state, nil
 }
+
+type Runner func(*State, *ImageSearch, Results) error
 
 type Behavior struct {
-	Tests []Test
-	Run   func(*State, *ImageSearch, []Result) error
+	AndTest
+	Run Runner
 }
 
-func (b *Behavior) Do(state *State, search *ImageSearch, cache map[string]Result) error {
-	r, ok := b.Test(search, cache)
-	if !ok {
+func (b Behavior) Do(state *State, search *ImageSearch, results Results) error {
+	if !b.Test(search, results) {
 		return nil
 	}
-	return b.Run(state, search, r)
+	return b.Run(state, search, results)
 }
 
-func (b *Behavior) Test(search *ImageSearch, cache map[string]Result) ([]Result, bool) {
-	r := make([]Result, len(b.Tests))
-	for ix, t := range b.Tests {
-		id := t.ID()
-		res, ok := cache[id]
-		if !ok {
-			res = t.Test(search)
-			cache[id] = res
-		}
-
-		if !res.Match {
-			return r, false
-		}
-		r[ix] = res
+func NewBehavior(Tests []Test, Run Runner) Behavior {
+	return Behavior{
+		AndTest: AndTest{TestGroup{Tests: Tests}},
+		Run:     Run,
 	}
-
-	return r, true
 }
